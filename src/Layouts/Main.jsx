58c414b5de588e676d46a498c22d3e5def40fc22
Mohidul Hasan <mahi110145@gmail.com>
2023-05-19 03:58:16 +0600

    Gallery section design complete

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
+import Banner from "../Pages/Shared/Banner/Banner";
+import Gallery from "../Pages/Shared/Gallery/Gallery";
 
 const Main = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Banner></Banner>
+      <Gallery></Gallery>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
---
+++ src/Pages/Shared/Banner/Banner.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <div
+        className="hero min-h-max"
+        style={{
+          backgroundImage: `url("https://i.ibb.co/JjWct64/Banner.jpg")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content py-32">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello Car Lovers</h1>
+            <p className="mb-5">
+              All of your stylish sports car is available here. We have the
+              production house, marketing team, brand ambasador and our unique
+              architect.
+            </p>
+            <button className="btn btn-primary">See All Car</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
