4e42e4bd450d5143fcd1b247467d0d25e267e548
Mohidul Hasan <mahi110145@gmail.com>
2023-05-19 02:55:38 +0600

    Login and registration page redesign complete

--- src/Pages/LoginRegistration/Login/Login.jsx
+++ src/Pages/LoginRegistration/Login/Login.jsx
@@ -4,51 +4,92 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="border w-2/3 mx-auto py-10 my-10">
-      <h2 className="text-5xl text-center border-b-2 w-1/2 mx-auto pb-8">
-        Login your account
-      </h2>
-      <form>
-        <div className="form-control w-full max-w-xs mx-auto">
-          <label className="label">
-            <span className="label-text font-semibold text-lg">
-              Email Address
-            </span>
-          </label>
-          <input
-            type="text"
-            placeholder="Type your email"
-            className="input input-bordered w-full max-w-xs"
+    <>
+      <div class="flex min-h-full flex-col justify-center px-6  lg:px-8 w-2/3 mx-auto py-5 mt-5 mb-10 border">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            class="mx-auto h-10 w-auto"
+            src="https://i.ibb.co/BzGdpKC/transparent-logo.png"
+            alt="Your Company"
           />
+          <h2 class="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
         </div>
-        <div className="form-control w-full max-w-xs mx-auto">
-          <label className="label">
-            <span className="label-text font-semibold text-lg">Password</span>
-          </label>
-          <input
-            type="password"
-            placeholder="Type your password"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </div>
-        <div className="mt-5 max-w-xs mx-auto">
-          <button className="btn w-full" type="submit">
-            Login
-          </button>
-          <button className="flex items-center gap-2 border-2 border-blue-400 p-2 rounded-lg mt-3 mx-auto">
-            <FaGoogle className="text-blue-500" />
-            Login with Google
-          </button>
-          <p className="text-center mt-2">
-            Don't have an account?{" "}
-            <Link className="text-blue-600" to="/register">
-              {" "}
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form class="space-y-6">
+            <div>
+              <label
+                for="email"
+                class="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div class="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  required
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div class="flex items-center justify-between">
+                <label
+                  for="password"
+                  class="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div class="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autocomplete="current-password"
+                  required
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p class="mt-2 text-center text-sm text-gray-500">
+            Don't have an account?
+            <Link
+              to="/register"
+              class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
               Register
             </Link>
           </p>
+          <div class="flex items-center my-4">
+            <div class="flex-grow border-t border-gray-300"></div>
+            <span class="px-3 text-gray-500">Or continue with</span>
+            <div class="flex-grow border-t border-gray-300"></div>
+          </div>
+          <button className="flex items-center justify-center gap-2 p-2 rounded-lg mt-3 mx-auto bg-black text-white w-2/3">
+            <FaGoogle className="" />
+            Google{" "}
+          </button>
         </div>
-      </form>
-    </div>
+      </div>
+    </>
   );
 };
 
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,12 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Login from "../Pages/LoginRegistration/Login/Login";
+import Register from "../Pages/LoginRegistration/Registration/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
-    children: [{ path: "/login", element: <Login></Login> }],
+    children: [
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 
